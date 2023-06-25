cf688850e6af37b71735fcf3b065718cee6b7a7d
Luana Ronau <luana.ronau.m@gmail.com>
2023-06-24 23:23:38 -0300

    yeah

---
+++ trabalho-pt2-nao_consigo_dar-push (1)/trabalho-pt2-nao_consigo_dar-push - Copia/controladores/controlador_fun_comum.py
@@ -0,0 +1,13 @@
+from exception.repeticao_exp import Repeticao
+from controladores.controlador_funcionario import ControladorFuncionario
+
+
+class ControladorFunComum(ControladorFuncionario):
+
+    def __init__(self):
+        super().__init__()
+        self.__fun_comum_dao = super().fun_comum_dao
+
+    @property
+    def fun_comum_dao(self):
+        return self.__fun_comum_dao
